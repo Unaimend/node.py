9d1502e31f7abd997f40c602437fe88c7e4f145b
Thomas Dost <dostthomas@icloud.com>
2019-02-11 20:41:15 +0100

    changed to console version

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from note_controller import NoteController
 from note_model import NoteModel
 from note_console_view import NoteConsoleView
 import logger
-from PySide2 import QtWidgets
-from qt_view import QtView
+# from PySide2 import QtWidgets
+# from qt_view import QtView
 import sys
 if __name__ == "__main__":
     MODEL = NoteModel()
-    app = QtWidgets.QApplication(sys.argv)
-    VIEW = QtView(model=MODEL)
-    VIEW.setMinimumSize(800, 800)
-    # CONTROLLER = NoteController(model=MODEL, view=VIEW)
+   # app = QtWidgets.QApplication(sys.argv)
+    VIEW = NoteConsoleView(model=MODEL)
+    #VIEW.setMinimumSize(800, 800)
+    CONTROLLER = NoteController(model=MODEL, view=VIEW)
     VIEW.run()
     # VIEW.subject_state = VIEW.State.SAVE_ALL, ""
     logger.fh.close()
-    sys.exit(app.exec_())
+   # sys.exit(app.exec_())
 
 
