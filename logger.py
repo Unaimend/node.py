ae45a7e5ef7a1e29024408929f02b072c81dfc2e
Thomas Dost <dostthomas@icloud.com>
2019-02-11 20:40:58 +0100

    Removed unecessary variables

--- logger.py
+++ logger.py
@@ -7,10 +7,7 @@ fh = logging.FileHandler('debug.log', mode="w")
 fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.ERROR)
 logger.addHandler(fh)
-logger.addHandler(ch)
 
 ERROR = logging.ERROR
 DEBUG = logging.DEBUG
